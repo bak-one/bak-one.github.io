ea96cc7b5611373c59a7bd9c935f603844fef50c
BlackoooWhite <homenkoekaterinaaa@gmail.com>
2018-04-16 11:45:33 +0300

    Demo

--- js/ontouch.js
+++ js/ontouch.js
@@ -1,11 +1,4 @@
 
-// $(function(){
-// 	if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-//   	$('body').addClass('touch');
-// 	}
-// });
-
-
 $(function(){
 	let deviceAgent = navigator.userAgent.toLowerCase();
 	let agentID = deviceAgent.match(/(iphone|ipod|ipad)/);
