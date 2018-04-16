871a75df6145a4a45c996ac537b32e9f5d3635f0
bak-one <38318599+bak-one@users.noreply.github.com>
2018-04-16 09:58:42 +0300

    Add files via upload

--- js/onclick.js
+++ js/onclick.js
@@ -1,7 +1,13 @@
-const btn = document.querySelector('.btn-nav');
-		const holder = document.body;
+// const btn = document.querySelector('.btn-nav');
+// 		const holder = document.body;
 	
-		btn.addEventListener('click', (e)=>{
-			e.preventDefault();
-			holder.classList.toggle('nav-active');
-		})
+// 		btn.addEventListener('click', (e)=>{
+// 			e.preventDefault();
+// 			holder.classList.toggle('nav-active');
+// 		})
+
+$(function(){
+	$('.btn-nav').click(function() {
+		$('body').toggleClass('nav-active');
+	})
+});
