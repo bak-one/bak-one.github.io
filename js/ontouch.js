a1c54abc332602b15dc0f8e0efd40044384e4b5f
BlackoooWhite <homenkoekaterinaaa@gmail.com>
2018-04-16 19:48:42 +0300

    Test

--- js/ontouch.js
+++ js/ontouch.js
@@ -1,8 +1,15 @@
 
-$(function(){
-	let deviceAgent = navigator.userAgent.toLowerCase();
-	let agentID = deviceAgent.match(/(iphone|ipod|ipad)/);
-	if (agentID) {
-		$('body').addClass('ios');
-	}
-});
+// $(function(){
+// 	if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+//   	$('body').addClass('touch');
+// 	}
+// });
+
+
+// $(function(){
+// 	let deviceAgent = navigator.userAgent.toLowerCase();
+// 	let agentID = deviceAgent.match(/(iphone|ipod|ipad)/);
+// 	if (agentID) {
+// 		$('body').addClass('ios');
+// 	}
+// });
